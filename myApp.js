20c7f4a39f331ca330ce2ee4c26d84669fdb117b
Replit user <>
2023-06-28 01:55:42 +0000

    chore: Create a person schema called personSchema with the following shape:
    - A required name field of type String
    - An age field of type Number
    - A favoriteFoods field of type [String]
    
    Now, create a model from the personSchema and assign it to the existing variable Person

--- myApp.js
+++ myApp.js
@@ -1,9 +1,9 @@
-mongoose = require('mongoose')
+mongoose = require('mongoose');
 require('dotenv').config();
 
 mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true });
 
-let Person;
+let Person = require('./models/person');
 
 const createAndSavePerson = (done) => {
   done(null /*, data*/);
